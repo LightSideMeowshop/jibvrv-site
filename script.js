a1e18e3dc1f4019feb9eaf99fad53369afce812b
Malvis Light <malvislight@gmail.com>
2025-02-16 11:53:23 +0400

    Update site design❤️

--- script.js
+++ script.js
@@ -11,15 +11,13 @@ fetch('singles.json')
 
             // Добавляем обложку с кнопкой для перехода на страницу сингла
             singleDiv.innerHTML = `
-                <a href="single?single=${encodeURIComponent(single.title)}">
-                    <img src="${single.cover}" alt="${single.title}">
+                <a href="${single.link}">
+                    <div class="masked-wrapper">
+                        <img src="${single.cover}" alt="${single.title}" class="masked">
+                    </div>
                 </a>
                 <div class="description">
                     <h2>${single.title}</h2>
-                    <p>${single.artist}</p>
-                </div>
-                <div class="platforms">
-                    ${generatePlatformLinks(single.links)}
                 </div>
             `;
 
@@ -27,23 +25,3 @@ fetch('singles.json')
         });
     })
     .catch(error => console.error('Error loading singles:', error));
-
-// Генерация ссылок на платформы
-function generatePlatformLinks(links) {
-    const platforms = {
-        spotify: '<i class="fab fa-spotify"></i>',
-        apple: '<i class="fab fa-apple"></i>',
-        youtube: '<i class="fab fa-youtube"></i>',
-        yandex: '<i class="fas fa-music"></i>',
-        vk: '<i class="fab fa-vk"></i>',
-        tiktok: '<i class="fab fa-tiktok"></i>'
-    };
-
-    return Object.keys(links)
-        .map(platform => `
-            <a href="${links[platform]}" class="${platform}">
-                ${platforms[platform]} ${platform.charAt(0).toUpperCase() + platform.slice(1)}
-            </a>
-        `)
-        .join('');
-}
